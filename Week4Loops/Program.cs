60b1af785e91ff44a9fdc18a72ea9a3ff03eba68
Marin Salumets <MarinSalumets@marru.lan>
2024-10-06 18:53:19 +0300

    add week4 to code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// FOR - LOOP
+// DRY - principle - Do Not Repeat Yourself
+
+for(int i = 0; i < 5; i++) //i++ - i + 1
+    Console.WriteLine("We don't need no education.");
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+
